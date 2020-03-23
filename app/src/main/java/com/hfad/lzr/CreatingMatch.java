6c8baabf888a7162a78659e41ca352410160331f
Mihailo Ljubinac <mihailoljubinac10@gmail.com>
2020-03-23 19:10:41 +0100

    pocetak

---
+++ app/src/main/java/com/hfad/lzr/CreatingMatch.java
@@ -0,0 +1,14 @@
+package com.hfad.lzr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreatingMatch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creating_match);
+    }
+}
